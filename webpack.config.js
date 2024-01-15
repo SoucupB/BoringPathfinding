2d97c38ea0923bcd0a59b32b696dba561e6083c2
SoucupB <bogdan.soucup@gmail.com>
2024-01-16 00:27:25 +0200

    First commit

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js', // Entry point of your application
+  output: {
+    filename: 'bundle.js', // Output bundle file name
+    path: path.resolve(__dirname, 'dist'), // Output directory
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/, // Match JavaScript files
+        exclude: /node_modules/, // Exclude node_modules directory
+        use: {
+          loader: 'babel-loader', // Use Babel for transpilation
+        },
+      },
+    ],
+  },
+};
