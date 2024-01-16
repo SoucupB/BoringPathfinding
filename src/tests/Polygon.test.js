0e08d1cd1ecad51757e3adea480ef36fc6c0215e
SoucupB <bogdan.soucup@gmail.com>
2024-01-17 01:12:50 +0200

    Add crossing line

--- src/tests/Polygon.test.js
+++ src/tests/Polygon.test.js
@@ -2,15 +2,15 @@ import tap from 'tap'
 const { test } = tap;
 import Polygon from '../sources/Polygon.js';
 
-// test('Push 2 lines', (t) => {
-//   const polygon = new Polygon();
-//   polygon.push(0, 0);
-//   polygon.push(0, 5);
-//   polygon.push(5, 5);
+test('Push 2 lines', (t) => {
+  const polygon = new Polygon();
+  polygon.push(0, 0);
+  polygon.push(0, 5);
+  polygon.push(5, 5);
 
-//   t.equal(polygon.linesCount(), 2, '2 lines');
-//   t.end();
-// });
+  t.equal(polygon.linesCount(), 2, '2 lines');
+  t.end();
+});
 
 test('Non crossing lines', (t) => {
   const polygon = new Polygon();
@@ -18,5 +18,15 @@ test('Non crossing lines', (t) => {
   for(let i = 0; i < points.length; i++) {
     t.equal(polygon.push(points[i][0], points[i][1]), true, `push point ${i}`);
   }
+  t.end();
+});
+
+test('First crossing lines', (t) => {
+  const polygon = new Polygon();
+  let points = [[0, 0], [0, 5], [5, 5], [3, 3]]
+  for(let i = 0; i < points.length; i++) {
+    polygon.push(points[i][0], points[i][1])
+  }
+  t.equal(polygon.push(3, 6), false, `cross line`);
   t.end();
 });
