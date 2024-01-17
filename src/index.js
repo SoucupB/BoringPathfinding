965d76c4437c8b0307b1f6a1ac06fdd9244b3c72
SoucupB <bogdan.soucup@gmail.com>
2024-01-17 19:23:08 +0200

    Correct canvas_DrawLine

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import Polygon from './sources/Geometry/Polygon.js'
+import canvas_DrawLine from './sources/Frontend/CanvasLine.js';
+import Point from './sources/Geometry/Point.js';
 
-const line = new Polygon();
-line.push(5, 4)
+// const line = new Polygon();
+// line.push(5, 4)
 
-line.display();
+// line.display();
+const canvas = document.getElementById('canvas-drawer');
+
+canvas_DrawLine(new Point(10, 50), new Point(50, 10), canvas);
---
+++ src/sources/Frontend/CanvasLine.js
@@ -0,0 +1,21 @@
+import Point from "../Geometry/Point.js"
+
+const canvas_DrawLine = (a, b, canvas = document.getElementById('canvas-drawer')) => {
+  const context = canvas.getContext('2d');
+  const height = canvas.height;
+  const width = canvas.width;
+
+  context.strokeStyle = '#00f'; // Blue color
+  context.lineWidth = 2;
+
+  const normalisedStart = new Point(a.y / 100.0 * height, a.x / 100.0 * width);
+  const normalisedEnd = new Point(b.y / 100.0 * height, b.x / 100.0 * width);
+
+  context.beginPath();
+  context.moveTo(normalisedStart.y, normalisedStart.x);
+  context.lineTo(normalisedEnd.y, normalisedEnd.x);
+  context.stroke();
+}
+
+// This canvas_DrawLine takes points that are normalised
+export default canvas_DrawLine;
